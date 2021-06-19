7c22645bea7f58eddb7ef68c0737a512d9b3a05e
SaKaarle <saku.kaarlejarvi@gmail.com>
2021-06-19 03:14:47 +0300

    Päivitetty, suojattu upload

--- ReadingFB.py
+++ ReadingFB.py
@@ -3,12 +3,14 @@ from pprint import pprint
 import json
 
 import tkinter
+import firebase_admin
+from firebase_admin import credentials
 
 
 #pip install pyrebase4
 
 config = {
-  "apiKey": "m2YYSDBUFvfB3AU7Px2mMoHtTUwRDOZhce6hgC38",
+  "apiKey": "AIzaSyC0gWWH779wuS1c8NuDHPF4TgwfcmlX5WQ",
   "authDomain": "temperatureproject-666.firebaseio.com",
   "databaseURL": "https://temperatureproject-666.firebaseio.com/",
   "projectID": "temperatureproject-666",
@@ -17,13 +19,43 @@ config = {
 # Ctrl+K+C, you'll comment out the section of code. Ctrl+K+U will uncomment the code
 # https://stackoverflow.com/questions/57256408/how-can-i-get-a-key-from-the-value-in-firebase
 firebase = pyrebase.initialize_app(config)
-# auth = firebase.auth()
+
+cred = credentials.Certificate('F:/code/Firebase/temperatureproject-666-firebase-admin.json')
+default_app = firebase_admin.initialize_app(cred,
+{'databaseURL': 'https://temperatureproject-666.firebaseio.com/'})
+
+auth = firebase.auth()
+
+email = input("Syötä säpö: ")
+password = input("Syötä salis: ")
+
+user = auth.sign_in_with_email_and_password(email, password)
+
+
 
 def noquote(s):
     return s
 pyrebase.pyrebase.quote = noquote
 
 db = firebase.database()
+''' Tässä tärkeää huomata, kun on suojattu datanlähettäminen, set(data,user['idToken']) 
+sallii datan lähetyksen serveriin JOS Firebasessa on annettu arvot:
+
+{
+  "rules": {
+    "DHT": {
+        ".read" : true,
+        ".write": "'$omauidtähän' === auth.uid"
+    }
+  }
+}
+
+Datan siirto Firebaseen ON TEORIASSA suojattu
+
+'''
+# data = {"name": "Mortimer 'Morty' Smith"}
+# db.child("users").child("Morty").set(data, user['idToken'])
+
 
 DateYearDay =  db.child("DHT").get()
 
@@ -40,3 +72,4 @@ for paiva in DateYearDay.each():
     
 
 #Tästä eteenpäin mars
+
